56586f74c957c11fc9b4bc8c243e1a4eff94c932
superdhebz <debiegraceb@yahoo.com>
2018-08-18 23:14:30 +0400

    backend

---
+++ db.js
@@ -0,0 +1,7 @@
+// db.js
+//astrolabs-debie debie123 mlab
+
+module.exports = {
+    //DB: 'mongodb://localhost:27017/auth'
+    DB: 'mongodb://astrolabs-debie:debie123@ds125272.mlab.com:25272/astrolabs-debie-final'
+}
